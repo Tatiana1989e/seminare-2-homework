395e3009bf86f996889cfd58d4612d9a69e22262
Egorova Tatiana <tanyasha.89@mail.ru>
2022-08-11 20:56:21 +0300

    seminars2homework task2task3

--- Program.cs
+++ Program.cs
@@ -1,6 +1,45 @@
-﻿// Задача 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+﻿/*
+// Задача 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 Console.Write("ThreeDigitNumbers: ");
 int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
 string stringNumber = Convert.ToString(threeDigitNumber);
 Console.WriteLine("secondNumber -> "+stringNumber[1]);
+*/
+/*
+// Задача 2. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Input number: ");
+int ThirdDigitNumber=Convert.ToInt32(Console.ReadLine());
+string ThirdDigitNumberText = Convert.ToString(ThirdDigitNumber());
+{
+    if (ThirdDigitNumberText.Length > 2);
+   
+     Console.WriteLine("third number -> " + ThirdDigitNumberText(2));
+
+     Console.WriteLine("-> third number no");
+   
+}
+*/
+/*
+// Задача 3. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("The number indicating the day of the week: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) 
+{
+  if (dayNumber < 6) 
+  {
+  Console.WriteLine("(is not weekend) -> no");
+  }
+  else if (dayNumber == 6 && dayNumber == 7);
+  {
+    Console.WriteLine("(weekend) -> yes");
+  }
+ 
+   
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
+*/
