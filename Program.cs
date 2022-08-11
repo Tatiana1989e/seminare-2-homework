93f68235e3aa7c4ef0dfa93703645851aa14394e
Egorova Tatiana <tanyasha.89@mail.ru>
2022-08-11 19:46:24 +0300

    seminars2homework task1

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("ThreeDigitNumbers: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("secondNumber -> "+stringNumber[1]);
